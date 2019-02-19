fb040437655b9bad98d88864620b05d9ef02ecb7
Stanislav Evseev <stanislav.evseev@kpn.com>
2019-02-20 00:07:03 +0100

    Initialization calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectionStrategy, ViewChild, TemplateRef} from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import {NgbdModalBasic} from "./modal-basic";
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calendar-slots2';
+  @ViewChild('modalContent') modalContent: TemplateRef<any>;
+  title = 'our magical calendar';
+  viewDate: Date = new Date();
+  events = [];
+
+  clickedDate: Date;
+
+
+  constructor(private modalService: NgbModal) {}
+
+  dayClickedHandler(day) {
+    this.clickedDate = day;
+    this.modalService.open(this.modalContent, { size: 'lg' });
+    console.log(day);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { CommonModule } from '@angular/common';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import {NgbModalBackdrop} from "@ng-bootstrap/ng-bootstrap/modal/modal-backdrop";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    RouterModule.forChild([{ path: '', component: AppComponent }]),
+    NgbModule.forRoot(),
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // providers: [
+  //   NgbModalBackdrop
+  // ],
+  bootstrap: [AppComponent],
+  exports: [AppComponent]
 })
 export class AppModule { }
